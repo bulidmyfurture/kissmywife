51137a9fa33e2d1012082a1f649c3f6187ca38e9
bulidmyfurture <46509629+bulidmyfurture@users.noreply.github.com>
2022-08-17 15:59:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,34 +15,41 @@ birthday = os.environ['BIRTHDAY']
 app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
 
-user_id = os.environ["USER_ID"]
+user_id = ["oYnjO6uwFCUxabv1khTNgb2-j5g8", "oYnjO6iKRJmszyyQGC3SKdm_Wah8"]
+
 template_id = os.environ["TEMPLATE_ID"]
 
 
 def get_weather():
-  url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
-  res = requests.get(url).json()
-  weather = res['data']['list'][0]
-  return weather['weather'], math.floor(weather['temp'])
+    url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
+    res = requests.get(url).json()
+    weather = res['data']['list'][0]
+    return weather['weather'], math.floor(weather['temp']), weather['wind'], weather['airQuality'], math.floor(
+        weather['high']), math.floor(weather['low'])
+
 
 def get_count():
-  delta = today - datetime.strptime(start_date, "%Y-%m-%d")
-  return delta.days
+    delta = today - datetime.strptime(start_date, "%Y-%m-%d")
+    return delta.days
+
 
 def get_birthday():
-  next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
-  if next < datetime.now():
-    next = next.replace(year=next.year + 1)
-  return (next - today).days
+    next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
+    if next < datetime.now():
+        next = next.replace(year=next.year + 1)
+    return (next - today).days
+
 
 def get_words():
-  words = requests.get("https://api.shadiao.pro/chp")
-  if words.status_code != 200:
-    return get_words()
-  return words.json()['data']['text']
+    words = requests.get("https://api.shadiao.pro/chp")
+    if words.status_code != 200:
+        return get_words()
+    return words.json()['data']['text']
+
 
 def get_random_color():
-  return "#%06x" % random.randint(0, 0xFFFFFF)
+    return "#%06x" % random.randint(0, 0xFFFFFF)
+
 
 def get_ciba():
     url = "http://open.iciba.com/dsapi/"
@@ -60,7 +67,15 @@ def get_ciba():
 client = WeChatClient(app_id, app_secret)
 note_ch, note_en = get_ciba()
 wm = WeChatMessage(client)
-wea, temperature = get_weather()
-data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()},"note_ch":{"value":note_ch},"note_en":{"value":note_en}}
-res = wm.send_template(user_id, template_id, data)
+wea, temperature, wind, airQuality, high, low = get_weather()
+data = {"weather": {"value": wea}, "city": {"value": city}, "temperature": {"value": temperature}, "wind": {"value": wind},
+        "low": {"value": low}, "high": {"value": high}, "airQuality": {"value": airQuality},
+        "love_days": {"value": get_count(), "color": get_random_color()},
+        "birthday_left": {"value": get_birthday(), "color": get_random_color()},
+        "words": {"value": get_words(), "color": get_random_color()},
+        "note_ch": {"value": note_ch, "color": get_random_color()},
+        "note_en": {"value": note_en, "color": get_random_color()}}
+res = wm.send_template(user_id[0], template_id, data)
+res1 = wm.send_template(user_id[1], template_id, data)
 print(res)
+print(res1)
